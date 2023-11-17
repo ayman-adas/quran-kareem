30659c5345969681118e7b5c9c45a7a3753126d2
ayman adas <139353510+ayman-adas@users.noreply.github.com>
2023-11-17 17:30:49 +0300

    Update w_app_bar.dart

--- quran_kareem/lib/Widget/w_app_bar.dart
+++ quran_kareem/lib/Widget/w_app_bar.dart
@@ -5,6 +5,7 @@ class WAppBar extends StatelessWidget implements PreferredSizeWidget {
   const WAppBar({super.key});
 
   @override
+  //app bar title and background color
   Widget build(BuildContext context) {
     return AppBar(
       leading: const Icon(Icons.menu),
